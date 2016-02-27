f04844ff3552eee83d4c61526d2eec43dc054ba4
RishabhKumar <rishabhkr1729@gmail.com>
2016-02-27 17:44:48 +0530

    loading gif added on homescreen.

--- app/src/main/java/com/productions/rk/tickitock/HomeScreen.java
+++ app/src/main/java/com/productions/rk/tickitock/HomeScreen.java
@@ -1,10 +1,12 @@
 package com.productions.rk.tickitock;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class HomeScreen extends Activity {
@@ -14,6 +16,10 @@ public class HomeScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        // loading gif
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("file:///android_asset/477.gif");
+
         // Setting custom font for welcome text
         TextView WelcomeText = (TextView)findViewById(R.id.welcomeId);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/3Dumb.ttf");
@@ -21,13 +27,13 @@ public class HomeScreen extends Activity {
 
         //Try Delay and Loading Property for HomeScreen
 
-       // 3s delay no animation
+       // 4s delay
         Handler handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(HomeScreen.this, OptionScreen.class));
             }
-        }, 3000);
+        }, 4000);
     }
 }
